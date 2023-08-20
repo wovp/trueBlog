6a6d2761b90f42b2fbe90037a2cb5f5d32b6a6e2
xiaolan <2801587874>
2023-08-20 11:45:28 +0800

    朋友圈定义功能

--- src/main/java/com/ourblog/blog/service/friendsInterface.java
+++ src/main/java/com/ourblog/blog/service/friendsInterface.java
@@ -1,5 +1,9 @@
 package com.ourblog.blog.service;
 
+import com.ourblog.blog.pojo.Blog;
+
+import java.util.List;
+
 /**
  * ClassName: friendsInterface
  * Package: com.ourblog.blog.service
@@ -10,4 +14,16 @@ package com.ourblog.blog.service;
 
 // 朋友圈的接口
 public interface friendsInterface {
+    // 展示部分文章列表,返回一个博客类
+    public List<Blog>getbloglist(String author_id,
+                                 String keyword,
+                                 String readed,
+                                 String likes,
+                                 String collects);
+
+    // 根据博主id获取博主个人界面
+    public Blog getauthordetail(String author_id);
+
+    // 根据博客标题获取博客详情内容,应该是博客的所有内容，也就是一个博客对象
+    public Blog getBlogDetail(String keyword);
 }
